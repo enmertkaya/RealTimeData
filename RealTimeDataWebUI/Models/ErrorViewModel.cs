cfd3b6e9e7285e6c216b4fff314c0c00eed78be3
Enes Mert Kaya <merteneskaya@outlook.com>
2023-11-27 15:41:02 +0300

    Add project files.

---
+++ RealTimeDataWebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RealTimeDataWebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
