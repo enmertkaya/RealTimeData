c4c7bf5d26f037ea23393293187d8cfbc8308a1c
Enes Mert Kaya <merteneskaya@outlook.com>
2023-11-28 14:07:26 +0300

    Manager devam

---
+++ RealTimeData.BusinessLayer/Concrete/ProductManager.cs
@@ -0,0 +1,40 @@
+﻿using RealTimeData.BusinessLayer.Abstract;
+using RealTimeData.DataAccessLayer.Abstract;
+using RealTimeData.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealTimeData.BusinessLayer.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        private readonly IProductDal _productDal;
+        public void TAdd(Product entity)
+        {
+            _productDal.Add(entity);
+        }
+
+        public void TDelete(Product entity)
+        {
+            _productDal.Delete(entity);
+        }
+
+        public Product TGetByID(int id)
+        {
+            return _productDal.GetByID(id); 
+        }
+
+        public List<Product> TGetListAll()
+        {
+            return _productDal.GetListAll();
+        }
+
+        public void TUpdate(Product entity)
+        {
+            _productDal.Update(entity);
+        }
+    }
+}
---
+++ RealTimeData.BusinessLayer/Concrete/SocialMediaManager.cs
@@ -0,0 +1,40 @@
+﻿using RealTimeData.BusinessLayer.Abstract;
+using RealTimeData.DataAccessLayer.Abstract;
+using RealTimeData.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealTimeData.BusinessLayer.Concrete
+{
+    public class SocialMediaManager : ISocialMediaService
+    {
+        private readonly ISocialMediaDal _socialMediaDal;
+        public void TAdd(SocialMedia entity)
+        {
+            _socialMediaDal.Add(entity);
+        }
+
+        public void TDelete(SocialMedia entity)
+        {
+            _socialMediaDal.Delete(entity);
+        }
+
+        public SocialMedia TGetByID(int id)
+        {
+            return _socialMediaDal.GetByID(id);
+        }
+
+        public List<SocialMedia> TGetListAll()
+        {
+            return _socialMediaDal.GetListAll();
+        }
+
+        public void TUpdate(SocialMedia entity)
+        {
+            _socialMediaDal.Update(entity);
+        }
+    }
+}
