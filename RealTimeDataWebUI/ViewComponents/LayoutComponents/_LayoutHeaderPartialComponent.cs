c74ee3a2c80783db052dceacfff412cc4f370946
Enes Mert Kaya <merteneskaya@outlook.com>
2023-11-30 11:49:03 +0300

    AdminLayout ve HeaderLayout eklendi

---
+++ RealTimeDataWebUI/ViewComponents/LayoutComponents/_LayoutHeaderPartialComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealTimeDataWebUI.ViewComponents.LayoutComponents
+{
+    public class _LayoutHeaderPartialComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
