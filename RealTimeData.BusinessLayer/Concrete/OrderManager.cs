d6a780d8a9fa4ad0b13acb858ef838fba05a8b78
Enes Mert Kaya <merteneskaya@outlook.com>
2023-12-13 18:42:29 +0300

    order ve orderdetail kisimlari mimari kismi

---
+++ RealTimeData.BusinessLayer/Concrete/OrderManager.cs
@@ -0,0 +1,46 @@
+﻿using RealTimeData.BusinessLayer.Abstract;
+using RealTimeData.DataAccessLayer.Abstract;
+using RealTimeData.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealTimeData.BusinessLayer.Concrete
+{
+    public class OrderManager : IOrderService
+    {
+        private readonly IOrderDal _orderDal;
+
+        public OrderManager(IOrderDal orderDal)
+        {
+            _orderDal = orderDal;
+        }
+
+        public void TAdd(Order entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Order entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Order TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Order> TGetListAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Order entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
