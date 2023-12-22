bfd8762791e99ea1edbd88c2199f6149e8993a3e
Enes Mert Kaya <merteneskaya@outlook.com>
2023-12-22 15:31:37 +0300

    signalR anlik istatistiklerin tamamlanmasi.

--- RealTimeData.Api/Hubs/SignalRHub.cs
+++ RealTimeData.Api/Hubs/SignalRHub.cs
@@ -9,12 +9,15 @@ namespace RealTimeData.Api.Hubs
         private readonly ICategoryService _categoryService;
         private readonly IProductService _productService;
         private readonly IOrderService _orderService;
-
-        public SignalRHub(ICategoryService categoryService, IProductService productService, IOrderService orderService)
+        private readonly IMoneyCaseService _moneyCaseService;
+        private readonly IMenuTableService _menuTableService;
+        public SignalRHub(ICategoryService categoryService, IProductService productService, IOrderService orderService, IMoneyCaseService moneyCaseService, IMenuTableService menuTableService)
         {
             _categoryService = categoryService;
             _productService = productService;
             _orderService = orderService;
+            _moneyCaseService = moneyCaseService;
+            _menuTableService = menuTableService;
         }
 
         public async Task SendStatistic()
@@ -55,6 +58,15 @@ namespace RealTimeData.Api.Hubs
 
             var value12 = _orderService.TActiveOrderCount();
             await Clients.All.SendAsync("ReceiveActiveOrderCount", value12);
+
+            var value13 = _orderService.TLastOrderPrice();
+            await Clients.All.SendAsync("ReceiveLastOrderPrice", value13.ToString("0.00" + "₺"));
+
+            var value14 = _moneyCaseService.TTotalMoneyCaseAmount();
+            await Clients.All.SendAsync("ReceiveTotalMoneyCaseAmount", value14.ToString("0.00" + "₺"));
+
+            var value16 = _menuTableService.TMenuTableCount();
+            await Clients.All.SendAsync("ReceiveMenuTableCount", value16);
 
 
         }
