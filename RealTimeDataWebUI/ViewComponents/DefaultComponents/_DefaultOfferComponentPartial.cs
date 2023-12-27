338c634474e7c289d5e4b1bd528d0842897fabe0
Enes Mert Kaya <merteneskaya@outlook.com>
2023-12-27 09:04:26 +0300

    slider ve offer alanlari eklendi

---
+++ RealTimeDataWebUI/ViewComponents/DefaultComponents/_DefaultOfferComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealTimeDataWebUI.ViewComponents.DefaultComponents
+{
+    public class _DefaultOfferComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke ()
+        {
+            return View();
+        }
+    }
+}
