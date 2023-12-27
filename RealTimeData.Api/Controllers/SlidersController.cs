40b8405d9a6f73cb3918e7a7c3de1c7133e87c2b
Enes Mert Kaya <merteneskaya@outlook.com>
2023-12-27 15:04:18 +0300

    slider api

---
+++ RealTimeData.Api/Controllers/SlidersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealTimeData.BusinessLayer.Abstract;
+
+namespace RealTimeData.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SlidersController : ControllerBase
+    {
+        private readonly ISliderService _sliderService;
+
+        public SlidersController(ISliderService sliderService)
+        {
+            _sliderService = sliderService;
+        }
+
+        [HttpGet]
+        public IActionResult SliderList()
+        {
+            return Ok(_sliderService.TGetListAll());
+        }
+    }
+}
