95bcbe83f88b79df9c059db2b119d36a0fc7df5f
Enes Mert Kaya <merteneskaya@outlook.com>
2024-01-03 09:44:59 +0300

    hakkimizda kismi ve referanslar kismi api consumeleri

--- RealTimeDataWebUI/ViewComponents/DefaultComponents/_DefaultAboutComponentPartial.cs
+++ RealTimeDataWebUI/ViewComponents/DefaultComponents/_DefaultAboutComponentPartial.cs
@@ -1,11 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealTimeDataWebUI.Dtos.AboutDtos;
+using RealTimeDataWebUI.Dtos.SliderDtos;
 
 namespace RealTimeDataWebUI.ViewComponents.DefaultComponents
 {
     public class _DefaultAboutComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke ()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultAboutComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7021/api/About");
+
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
+            return View(values);
+
             return View();
         }
     }
