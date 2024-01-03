b7ab51aa777dd7ec6ce818b3114c8375aa83c3cf
Enes Mert Kaya <merteneskaya@outlook.com>
2024-01-03 16:36:33 +0300

    footer alani eklendi

--- RealTimeDataWebUI/ViewComponents/UILayoutComponents/_UILayoutFooterComponentPartial.cs
+++ RealTimeDataWebUI/ViewComponents/UILayoutComponents/_UILayoutFooterComponentPartial.cs
@@ -1,12 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealTimeDataWebUI.Dtos.ContactDtos;
+using RealTimeDataWebUI.Dtos.SliderDtos;
 
 namespace RealTimeDataWebUI.ViewComponents.UILayoutComponents
 {
     public class _UILayoutFooterComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _UILayoutFooterComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7021/api/Contact");
+
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultContactDto>>(jsonData);
+            return View(values);
+
             return View();
         }
-    }
+}
 }
