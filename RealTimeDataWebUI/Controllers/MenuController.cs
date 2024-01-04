db4e45e05e6d4b628eb0a4a1bee35a43efca5bf4
Enes Mert Kaya <merteneskaya@outlook.com>
2024-01-04 18:25:18 +0300

    menude urunlerin listelenmesi

--- RealTimeDataWebUI/Controllers/MenuController.cs
+++ RealTimeDataWebUI/Controllers/MenuController.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealTimeDataWebUI.Dtos.ProductDtos;
 
 namespace RealTimeDataWebUI.Controllers
 {
     public class MenuController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public MenuController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7021/api/Product");
+
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+            return View(values);
+
             return View();
         }
     }
