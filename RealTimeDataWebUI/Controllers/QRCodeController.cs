bfeaf4e391a22f0cd1990a72e953b1490ad89ab9
Mert Kaya <merteneskaya outlook.com>
2024-01-11 19:45:01 +0300

    qrcode olusturuldu

---
+++ RealTimeDataWebUI/Controllers/QRCodeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealTimeDataWebUI.Controllers
+{
+	public class QRCodeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
