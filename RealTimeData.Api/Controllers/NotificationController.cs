f41322813facf6a4ec75085408490cd52e498056
Enes Mert Kaya <merteneskaya@outlook.com>
2024-01-12 17:37:01 +0300

    notification sinifi eklendi

---
+++ RealTimeData.Api/Controllers/NotificationController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealTimeData.BusinessLayer.Abstract;
+
+namespace RealTimeData.Api.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class NotificationController : ControllerBase
+	{
+		private readonly INotificationService _notificationService;
+		public NotificationController(INotificationService notificationService)
+		{
+			_notificationService = notificationService;
+		}
+
+		[HttpGet]
+		public IActionResult NotificationList()
+		{
+			return Ok(_notificationService.TGetListAll());
+		}
+	}
+}
