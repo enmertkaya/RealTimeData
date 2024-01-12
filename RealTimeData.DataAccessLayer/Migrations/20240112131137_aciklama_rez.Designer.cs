e44579559741dddf3d74da8ad0d8d45330f224e4
Enes Mert Kaya <merteneskaya@outlook.com>
2024-01-12 16:44:06 +0300

    rezervasyon listesinin signalR ile tabloya cekilmesi

--- RealTimeData.DataAccessLayer/Migrations/20240112131137_aciklama_rez.Designer.cs
+++ RealTimeData.DataAccessLayer/Migrations/20240112131137_aciklama_rez.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RealTimeData.DataAccessLayer.Concrete;
 
@@ -11,9 +12,10 @@ using RealTimeData.DataAccessLayer.Concrete;
 namespace RealTimeData.DataAccessLayer.Migrations
 {
     [DbContext(typeof(RealTimeDataContext))]
-    partial class RealTimeDataContextModelSnapshot : ModelSnapshot
+    [Migration("20240112131137_aciklama_rez")]
+    partial class aciklama_rez
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RealTimeData.DataAccessLayer/Migrations/20240112131137_aciklama_rez.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RealTimeData.DataAccessLayer.Migrations
+{
+    public partial class aciklama_rez : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Bookings",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Bookings");
+        }
+    }
+}
