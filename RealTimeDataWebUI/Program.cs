7f7b034a38858bdf54e6d82add9dc4f3d0e46a37
Enes Mert Kaya <merteneskaya@outlook.com>
2024-01-19 16:33:07 +0300

    authentication

--- RealTimeDataWebUI/Program.cs
+++ RealTimeDataWebUI/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using RealTimeData.DataAccessLayer.Concrete;
 using RealTimeData.EntityLayer.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
+var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+
 
 builder.Services.AddHttpClient();
 
@@ -9,7 +13,11 @@ builder.Services.AddHttpClient();
 builder.Services.AddHttpClient();
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<RealTimeDataContext>();
-builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<RealTimeDataContext>();
+builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<RealTimeDataContext>(); 
+builder.Services.AddControllersWithViews(opt =>
+{
+    opt.Filters.Add(new AuthorizeFilter(requireAuthorizePolicy));
+});
 
 
 var app = builder.Build();
