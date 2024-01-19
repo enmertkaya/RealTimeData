000b7589ab0245a43d11842f273ef13da5ccca8b
Enes Mert Kaya <merteneskaya@outlook.com>
2024-01-19 13:32:33 +0300

    message sinifi mimariye eklendi / api testi

---
+++ RealTimeData.BusinessLayer/Abstract/IMessageService.cs
@@ -0,0 +1,13 @@
+﻿using RealTimeData.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealTimeData.BusinessLayer.Abstract
+{
+    public interface IMessageService :IGenericService <Message>
+    {
+    }
+}
---
+++ RealTimeData.BusinessLayer/Concrete/MessageManager.cs
@@ -0,0 +1,47 @@
+﻿using RealTimeData.BusinessLayer.Abstract;
+using RealTimeData.DataAccessLayer.EntityFramework;
+using RealTimeData.EntityLayer.Entities;
+using RealTimeData.DataAccessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealTimeData.BusinessLayer.Concrete
+{
+    public class MessageManager : IMessageService
+    {
+        private readonly IMessageDal _messageDal;
+
+        public MessageManager(IMessageDal messageDal)
+        {
+            _messageDal = messageDal;
+        }
+
+        public void TAdd(Message entity)
+        {
+            _messageDal.Add(entity);
+        }
+
+        public void TDelete(Message entity)
+        {
+            _messageDal.Delete(entity);
+        }
+
+        public Message TGetByID(int id)
+        {
+            return _messageDal.GetByID(id);
+        }
+
+        public List<Message> TGetListAll()
+        {
+            return _messageDal.GetListAll();
+        }
+
+        public void TUpdate(Message entity)
+        {
+            _messageDal.Update(entity);
+        }
+    }
+}
