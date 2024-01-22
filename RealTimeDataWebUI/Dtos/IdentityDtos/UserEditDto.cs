8f1240ea2d0c5b28e23f9e51849e50d98e9a2492
Enes Mert Kaya <merteneskaya@outlook.com>
2024-01-22 14:15:14 +0300

    bilgileri guncelleme sayfasi eklendi

---
+++ RealTimeDataWebUI/Dtos/IdentityDtos/UserEditDto.cs
@@ -0,0 +1,13 @@
+﻿namespace RealTimeDataWebUI.Dtos.IdentityDtos
+{
+    public class UserEditDto
+    {
+        public string Mail { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
+    
